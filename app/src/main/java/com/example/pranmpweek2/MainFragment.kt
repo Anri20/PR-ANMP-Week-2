0bed5413a4877d12ceffb05e4e96ede755220d81
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-02-24 07:55:23 +0700

    add btnAction & btnSoftBack

--- app/src/main/java/com/example/pranmpweek2/MainFragment.kt
+++ app/src/main/java/com/example/pranmpweek2/MainFragment.kt
@@ -5,14 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
 
 class MainFragment : Fragment() {
+    private lateinit var view: View;
+    private lateinit var txtQuestion: TextView;
+    private lateinit var btnSubmit: Button;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        txtQuestion = view.findViewById(R.id.txtQuestion)
+        btnSubmit = view.findViewById(R.id.btnSubmit)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnSubmit.setOnClickListener {
+//            val answerTrue = MainFragmentDirections.actionAnswerTrue()
+//            Navigation.findNavController(it).navigate(answerTrue)
+            val gameover = MainFragmentDirections.actionResultFragment()
+            Navigation.findNavController(it).navigate(gameover)
+        }
     }
 }
--- app/src/main/java/com/example/pranmpweek2/ResultFragment.kt
+++ app/src/main/java/com/example/pranmpweek2/ResultFragment.kt
@@ -5,14 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 class ResultFragment : Fragment() {
+    private lateinit var view: View;
+    private lateinit var btnBack: Button;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        view = inflater.inflate(R.layout.fragment_result, container, false)
+
+        btnBack = view.findViewById(R.id.btnBack)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnBack.setOnClickListener {
+            val action = ResultFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
